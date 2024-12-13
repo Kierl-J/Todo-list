5af6c6aeb5340e888b6d210b010b9e676c9b0adb
Kierl-J <kierljayarinot94@gmail.com>
2024-12-13 11:35:08 +0800

    Added src files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-light">
+      <div className="container text-center">
+        <p className="mb-0">...</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
